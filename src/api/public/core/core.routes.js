aa0b78849076ecef74b24e0ec29d363f4576b03b
Srihari Vasipalle <104147710+vsrihari9@users.noreply.github.com>
2022-10-10 11:10:19 -0400

    cleanup repo

--- src/api/public/core/core.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const appController = require('./core.controller');
-
-const router = express.Router();
-
-router.route('/healthcheck').get(appController.healthCheckStatus);
-
-module.exports = router;
--- src/tests/perf/auth_helper.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-param-reassign */
-const jwtGenerator = require('../../api/shared/utils/JWTGenerator');
-
-exports.setupAuthToken = async (context, ee, next) => {
-  context.vars.AUTH_TOKEN = await jwtGenerator.generateJWT();
-  return next();
-};
