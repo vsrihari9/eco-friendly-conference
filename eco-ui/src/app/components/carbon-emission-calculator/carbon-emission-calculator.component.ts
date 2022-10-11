3e2c397d0b931f2712168cb1cf0f25fe99db9343
karakapavani <pavani.karaka@softrams.com>
2022-10-11 12:44:21 -0400

    add: added UI, styles for table and download reports functionality

--- eco-ui/src/app/components/carbon-emission-calculator/carbon-emission-calculator.component.ts
+++ eco-ui/src/app/components/carbon-emission-calculator/carbon-emission-calculator.component.ts
@@ -1,13 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators} from '@angular/forms';
+import {MatSort, Sort} from '@angular/material/sort';
 import {AppService} from "../../services/app.service";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+import {MatTableDataSource} from "@angular/material/table";
+import {MatTableExporterDirective} from "mat-table-exporter";
+
+export interface EffectiveCostElement {
+  dest_city_id: number;
+  city: string;
+  state: string;
+  code: number;
+  airfare: number;
+  co2: number;
+  perdiem: number;
+  monetaryCost: number;
+  effectiveCost: number;
+}
+
+let ELEMENT_DATA: EffectiveCostElement[] = [];
 
 @Component({
   selector: 'app-carbon-emission-calculator',
   templateUrl: './carbon-emission-calculator.component.html',
   styleUrls: ['./carbon-emission-calculator.component.css']
 })
-export class CarbonEmissionCalculatorComponent implements OnInit {
+export class CarbonEmissionCalculatorComponent implements OnInit, AfterViewInit {
 
   ecoForm: UntypedFormGroup = new UntypedFormGroup({});
 
@@ -22,7 +40,13 @@ export class CarbonEmissionCalculatorComponent implements OnInit {
   minDate: Date;
   maxDate: Date;
 
-  constructor(private formBuilder: UntypedFormBuilder, private service: AppService) {
+  displayedColumns: string[] = ['dest_city_id', 'city', 'state', 'code', 'airfare', 'co2', 'perdiem', 'monetaryCost', 'effectiveCost'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+  @ViewChild(MatSort, {static: false}) sort!: MatSort;
+  @ViewChild("exporter") exporter! : MatTableExporterDirective;
+
+  constructor(private formBuilder: UntypedFormBuilder, private service: AppService, private _liveAnnouncer: LiveAnnouncer) {
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
     const currentDate = new Date().getDate();
@@ -37,6 +61,12 @@ export class CarbonEmissionCalculatorComponent implements OnInit {
       this.cities = res.results;
     })
     this.createForm();
+  }
+
+  ngAfterViewInit() {
+    if(this.dataSource) {
+      this.dataSource.sort = this.sort;
+    }
   }
 
   createForm() {
@@ -60,6 +90,9 @@ export class CarbonEmissionCalculatorComponent implements OnInit {
         this.results = res.results;
         this.cols = [];
         if (this.results && this.results.length > 0) {
+          this.dataSource.data = this.results;
+          console.log('this.dataSource val is.... ', this.dataSource);
+          console.log('this.dataSource val is.... ', this.dataSource.data?.length);
           for (const col in this.results[0]) {
             this.cols.push({
               field: col,
@@ -84,6 +117,19 @@ export class CarbonEmissionCalculatorComponent implements OnInit {
         this.validateAllFormFields(control);
       }
     });
+  }
+
+  /** Announce the change in sort state for assistive technology. */
+  announceSortChange(sortState: Sort) {
+    // This example uses English messages. If your application supports
+    // multiple language, you would internationalize these strings.
+    // Furthermore, you can customize the message to add additional
+    // details about the values being sorted.
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
